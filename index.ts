5f6d60e07fc1c905aab107d3024244589d94cdbe
Maciej Palmowski <m.palmowski@freshpixels.pl>
2022-11-14 12:04:31 +0100

    fixes

--- index.ts
+++ index.ts
@@ -1,6 +1,11 @@
 import { serve } from "https://deno.land/std@0.57.0/http/server.ts";
+import { parse } from 'https://deno.land/std/flags/mod.ts';
 
-const s = serve({ port: 8000 });
+const { args } = Deno;
+const DEFAULT_PORT = 8000;
+const argPort = parse(args).port;
+
+const s = serve({ port: argPort ? Number(argPort) : DEFAULT_PORT });
 
 for await (const req of s) {
   req.respond({ body: "Hello World\n" });
